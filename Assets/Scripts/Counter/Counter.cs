3743758b2f0bf26e4ca1e1b9e242a93912cce364
Kislota_RU <tsepilov.matvey@mail.ru>
2024-10-21 18:52:01 +0300

    Add file Counter

---
+++ Assets/Scripts/Counter/Counter.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    [SerializeField] private float _delay = 0.5f;
+    [SerializeField] private float _stepToChange = 1f;
+
+    private float _scores;
+    private bool _isMouseDown;
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Êëèê {_isMouseDown}");
+
+        if (_isMouseDown == true)
+        {
+            _isMouseDown = false;
+        }
+        else
+        {
+            _isMouseDown = true;
+            StartCoroutine(nameof(IncreaseScores));
+        }
+    }
+
+    private IEnumerator IncreaseScores()
+    {
+        var wait = new WaitForSeconds(_delay);
+
+        while (_isMouseDown == true)
+        {
+            AddScores(_stepToChange);
+            yield return wait;
+        }
+    }
+
+    private void AddScores(float scores)
+    {
+        if (scores > 0)
+        {
+            _scores += scores;
+            Debug.Log($"Ñ÷¸ò: {_scores}");
+        }
+    }
+}
---
+++ Assets/Scripts/For Test/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private const string Horizontal = nameof(Horizontal);
+    private const string Vertical = nameof(Vertical);
+
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _moveSpeed;
+
+    private void Update()
+    {
+        Move();
+        Rotate();
+    }
+
+    private void Move()
+    {
+        float direction = Input.GetAxis(Vertical);
+        float distance = direction * _moveSpeed * Time.deltaTime;
+
+        transform.Translate(Vector3.forward * distance);
+    }
+
+    private void Rotate()
+    {
+        float rotation = Input.GetAxis(Horizontal);
+        
+        transform.Rotate(Vector3.up * rotation * _rotateSpeed * Time.deltaTime);
+    }
+}
