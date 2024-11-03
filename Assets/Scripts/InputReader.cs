cb097c67da2a89198e73b86435b26a5ffed88d3a
Kislota_RU <tsepilov.matvey@mail.ru>
2024-11-03 11:56:23 +0300

    Update Task Explosions Cubes 2

--- Assets/Scripts/InputReader.cs
+++ Assets/Scripts/InputReader.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class InputReader : MonoBehaviour
 {
+    private readonly int _numberButton = 0;
+
+    [SerializeField] private Camera _camera;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0) == false)
+        if (Input.GetMouseButtonDown(_numberButton) == false)
             return;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out RaycastHit hit) == false)
             return;
