ccdddbc36277f460bfd33bbed3738cbbc42f90d2
Kislota_RU <tsepilov.matvey@mail.ru>
2025-04-03 22:35:15 +0300

    Task (Audio Mixer)

--- Assets/Scripts/Mechanics/Health/Damager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Damager : MonoBehaviour
-{
-    [SerializeField] private int _damageValue;
-
-    public void Attack(Health targetHealth) =>
-        targetHealth.TakeDamage(_damageValue);
-}
--- Assets/Scripts/Mechanics/Health/Healer.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Healer : MonoBehaviour
-{
-    [SerializeField] private int _healValue;
-
-    public void Heal(Health targetHealth) =>
-        targetHealth.TakeHealth(_healValue);
-}
